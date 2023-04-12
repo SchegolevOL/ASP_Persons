c162d484c6b87b6bf572c0b891e2f48eb0beb97d
Oleg Schegol <olschegolev@yahoo.com>
2023-04-12 15:49:31 +0500

    Add project files.

---
+++ ASP_Persons/Migrations/20230412080511_Init.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP_Persons.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "int", maxLength: 3, nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateAddition = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ ASP_Persons/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Persons.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Не ввели Имя"), MaxLength(50), MinLength(2)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Не ввели Фамилия"), MaxLength(50), MinLength(2)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Не ввели возраст"), MaxLength(3), MinLength(1)]
+        public int Age { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        public DateTime DateAddition { get; set;}
+    }
+}
